d41fe0ceace94cd023f8a724ddf61e6b8f2fcdec
ben <bnarvan@gmail.com>
2023-05-14 23:25:15 -0700

    working backtest

--- backtest.py
+++ backtest.py
@@ -1,25 +1,24 @@
+from datetime import timedelta
 from os.path import exists
 
 import numpy as np
 import pandas as pd
+from ffn import calc_stats
 from pyrb import RiskBudgeting
+from pyfolio import create_full_tear_sheet, create_returns_tear_sheet, create_simple_tear_sheet
 
 from yahoo_data import ASSET_PRICES_FILE, DATA_DIR, get_tickers
 
-DEFAULT_START_DATE = "1993-04-28"
-
-DEFAULT_END_DATE = "2022-11-10"
-
 #  [Risk weight for stocks, bonds, and commodities]
-POLICY_RISK_BUDGET = [.8, .1, .1]
+POLICY_RISK_BUDGET = [.6, .2, .2]
 
 TARGET_WEIGHTS_FILE = f"{DATA_DIR}/target_weights.csv"
 
 
 def get_returns():
     prices = pd.read_csv(ASSET_PRICES_FILE)
-    returns = np.log(prices[get_tickers()] / prices.shift(1)[get_tickers()]).dropna()
-    returns["Date"] = pd.to_datetime(prices["Date"]).dt.to_period(freq="D")
+    returns = prices[get_tickers()].pct_change()
+    returns["Date"] = pd.to_datetime(prices["Date"])#.dt.to_period(freq="D")
     returns.set_index("Date", inplace=True)
     return returns
 
@@ -36,12 +35,29 @@ def calculate_weights(cov):
 def generate_portfolio_weights(look_back_period=60):
     if not exists(TARGET_WEIGHTS_FILE):
         returns = get_returns()
-        returns.head()
         cov = returns.rolling(f"{look_back_period}D").cov().dropna()
         weights = pd.DataFrame(columns=get_tickers())
         weights[get_tickers()] = cov.groupby(level=0).apply(
             lambda c: pd.Series([weight for weight in calculate_weights(c)])
         )
-        weights.head()
         weights.to_csv(TARGET_WEIGHTS_FILE)
     return pd.read_csv(TARGET_WEIGHTS_FILE, index_col="Date", parse_dates=["Date"])
+
+
+def calculate_portfolio_returns(look_back_period=60, holding_period=20):
+    returns = get_returns()
+    weights = generate_portfolio_weights(look_back_period).shift(1)
+    #returns = returns.resample(f"{holding_period}D").sum()
+    #returns = np.exp(returns) - 1
+    #weights = returns.resample(f"{holding_period}D").mean().shift(1)
+    portfolio_returns = []
+    for index, row in returns[2:].iterrows():
+        s = 0
+        for ticker in get_tickers():
+            s += row[ticker] * weights.loc[index][ticker]
+        portfolio_returns.append({"Date": index, "Portfolio_return": s})
+    portfolio_returns = pd.DataFrame(portfolio_returns).set_index(["Date"])
+    strat_price = 1.0 * (1 + portfolio_returns["Portfolio_return"]).cumprod()
+    strat_price.index = strat_price.index.astype('datetime64[ns]')
+    calc_stats(strat_price).display()
+    return portfolio_returns
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from backtest import generate_portfolio_weights
+from backtest import generate_portfolio_weights, calculate_portfolio_returns
 from portfolio import run_back_test
 from yahoo_data import download_asset_class_data_yahoo
 import time
 
 if __name__ == '__main__':
     st = time.time()
-    weights = generate_portfolio_weights()
+    calculate_portfolio_returns()
     et = time.time()
     print(f"Took {et - st} seconds to generate portfolio weights")
